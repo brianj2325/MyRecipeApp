6889ac3ca68c55a33229c38c62ea31045e1f96ef
Brian Jacobson <38300827+brianj2325@users.noreply.github.com>
2019-03-16 09:18:22 -0500

    Add files via upload

---
+++ AddRecipeForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyRecipesApp
+{
+    public partial class AddRecipeForm : Form
+    {
+        public AddRecipeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Next_Click(object sender, EventArgs e)
+        {
+            RecipeDataSet ds = new RecipeDataSet();
+
+            DataRow row = ds.Tables["recipeTable"].NewRow();
+            row["recipeName"] = txt_RecipeName.Text;
+            row["recipeCategory"] = cmb_Category.Text;
+            row["recipeDescription"] = txt_Description.Text;
+            ds.RecipeTable.Rows.Add(row);
+
+            AddIngredientsForm addIngredientsForm = new AddIngredientsForm(row["recipeID"].ToString(),txt_RecipeName.Text);
+            this.Hide();
+            addIngredientsForm.ShowDialog();
+            this.Close();
+        }
+
+        private void btn_Cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+       
+    }
+
+    
+
+    
+}
