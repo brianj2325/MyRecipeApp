d087c358e117dff27b1e625281331928d45755d0
Brian Jacobson <38300827+brianj2325@users.noreply.github.com>
2019-03-25 10:14:26 -0500

    Get recipe ID for new Recipe

--- MyRecipesApp/MyRecipesApp/AddRecipeForm.cs
+++ MyRecipesApp/MyRecipesApp/AddRecipeForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,22 +13,33 @@ namespace MyRecipesApp
 {
     public partial class AddRecipeForm : Form
     {
+        string connectionString = @"Data Source = OfficeComputer\SQLEXPRESS; Initial Catalog = MyRecipeAppDB; Integrated Security=True;";
+        DataSet recipeData = new DataSet("RecipeData");
+        DataTable recipeTable = new DataTable("recipeTable");
+        int recipeID;
+        
+        
+
         public AddRecipeForm()
         {
             InitializeComponent();
+              
+            // Add recipeTable to recipeData dataset
+            recipeTable = recipeData.Tables.Add("recipeTable");
+            // Fill recipeTable with recipes from SQL database
+            recipeTable = fillDataTable(recipeTable);
         }
 
         private void btn_Next_Click(object sender, EventArgs e)
         {
-            RecipeDataSet ds = new RecipeDataSet();
+            
+            //string recipeID="";
+            AddRow();
 
-            DataRow row = ds.Tables["recipeTable"].NewRow();
-            row["recipeName"] = txt_RecipeName.Text;
-            row["recipeCategory"] = cmb_Category.Text;
-            row["recipeDescription"] = txt_Description.Text;
-            ds.RecipeTable.Rows.Add(row);
-
-            AddIngredientsForm addIngredientsForm = new AddIngredientsForm(row["recipeID"].ToString(),txt_RecipeName.Text);
+            PopulateDatabase();
+            
+               
+            AddIngredientsForm addIngredientsForm = new AddIngredientsForm(recipeID, txt_RecipeName.Text);
             this.Hide();
             addIngredientsForm.ShowDialog();
             this.Close();
@@ -38,10 +50,88 @@ namespace MyRecipesApp
             this.Close();
         }
 
-       
+
+        public DataTable fillDataTable(DataTable table)
+        {
+            
+            using (SqlConnection sqlConn = new SqlConnection(connectionString))
+            {
+                sqlConn.Open();
+                string query = "SELECT * FROM [" + table + "] ORDER BY recipeID DESC";
+                
+
+                SqlCommand cmd = new SqlCommand(query, sqlConn);
+
+                
+                using (SqlDataAdapter a = new SqlDataAdapter(cmd))
+                {
+                    a.Fill(recipeTable);
+                    sqlConn.Close();
+                    return recipeTable;
+                }
+            }
+
+         
+        }
+
+        public void clearDatabase()
+        {
+
+        }
+
+        public void PopulateDatabase()
+        {
+            
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+            using (var sqlBulk = new SqlBulkCopy(connectionString))
+            {
+                sqlBulk.DestinationTableName = "RecipeTable";
+                sqlBulk.WriteToServer(recipeTable);
+            }
+        }
+        
+        public void AddRow()
+        {
+            DataRow row = recipeData.Tables["recipeTable"].NewRow();
+
+            row["recipeID"] = GetRecipeID()+1;
+            row["recipeName"] = txt_RecipeName.Text;
+            row["recipeCategory"] = cmb_Category.Text;
+            row["recipeDescription"] = txt_Description.Text;
+            recipeTable.Rows.Add(row);
+
+        }
+
+        public int GetRecipeID()
+        {
+            if (recipeTable != null)
+            {
+                if (recipeTable.Rows.Count > 0)
+                {
+                    recipeID = Convert.ToInt32(recipeTable.Rows[0]["recipeID"]);
+
+                }
+
+                else
+                {
+                    recipeID = 1;
+                }
+
+            }
+          
+                   
+            
+            
+            return recipeID;
+        }
+
+
+
+
     }
 
-    
 
-    
+
+
 }
